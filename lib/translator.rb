53d140a6760b87e661815695e242a52a521cc3a1
Matt Choi <choim3@spu.edu>
2020-11-13 20:55:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,35 @@
 # require modules here
+require 'pry'
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(file_name)
+  file_list = YAML.load_file(file_name)
+  emoji_hash= {}
+  
+  file_list.each do |meaning, emojis|
+    
+    emoji_hash ["#{meaning}"] = {:english => emojis[0]}
+    # emoji_hash ["emoticon_symbol"][emojis[0]] = emojis[1]
+    
+  end
+  binding.pry
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_name, emoji)
+  
+  file_list = load_library(file_name)
+    if file_list["emoticon_symbol"][emoji]
+      return file_list["emoticon_symbol"][emoji]
+    else 
+      p "Sorry, that emoticon was not found"
+    end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_name, emoji)
+  file_list = load_library(file_name)
+    if file_list["emoticon_meaning"][emoji]
+     return file_list["emoticon_meaning"][emoji]
+   else
+     p "Sorry, that emoticon was not found"
+    end 
 end
